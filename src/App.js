199c6ae8168e3f616bde562c2316d76d2c2658bf
aloaloalo1703 <hung17039696@gmail.com>
2023-09-30 13:25:02 +0700

    Buoi14

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Converter from './Components/Converter/Converter';
+import Buoi14 from './Components/Buoi14/Buoi14';
 
 function App() {
   return (
     <div className='App'>
       <div className='converter'>
-<Converter />
-    </div>
+        <Buoi14 />
+      </div>
     </div>
   );
 }
